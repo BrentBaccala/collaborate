f4305c550cccd09adf8b429279022b043e56e3b1
Brent Baccala <cosine@freesoft.org>
2020-04-28 19:55:17 -0400

    move freeswitch code to an import file

---
+++ freeswitch.py
@@ -0,0 +1,75 @@
+
+import subprocess
+import json
+
+FS_CLI = "/opt/freeswitch/bin/fs_cli"
+
+freeswitch_ids = {}
+conference = None
+
+def get_freeswitch_ids():
+
+    #
+    # Fetch freeswitch conference data and map names to freeswitch id numbers
+    #
+    # We ONLY save student ids, because we don't want to mute/unmute teachers at all
+    #
+
+    global conference
+
+    freeswitch_process = subprocess.Popen([FS_CLI, '-x', 'conference json_list'], stdout=subprocess.PIPE)
+    (stdoutdata, stderrdata) = freeswitch_process.communicate()
+    try:
+        conference = json.loads(stdoutdata.decode())
+    except:
+        conference  = []
+
+    freeswitch_ids.clear()
+
+    if len(conference) > 0:
+        for member in conference[0]['members']:
+            try:
+                member_name = member['caller_id_name'].split('-bbbID-')[1]
+                id = member['id']
+                if 'DCPS' not in member_name:
+                    freeswitch_ids[member_name] = id
+            except:
+                pass
+
+get_freeswitch_ids()
+
+def freeswitch_cmd(cmd):
+    freeswitch_process = subprocess.Popen([FS_CLI, '-x', cmd])
+    freeswitch_process.wait()
+
+def freeswitch_conference_cmd(*cmd):
+    freeswitch_cmd('conference ' + conference[0]['conference_name'] + ' ' + ' '.join(map(str,cmd)))
+
+def freeswitch_set_broadcast():
+    for id in freeswitch_ids.values():
+        freeswitch_conference_cmd('undeaf', id)
+        freeswitch_conference_cmd('unmute', id)
+
+def freeswitch_set_silence():
+    for id in freeswitch_ids.values():
+        freeswitch_conference_cmd('deaf', id)
+        freeswitch_conference_cmd('mute', id)
+
+def freeswitch_set_private(student_name):
+    for id in freeswitch_ids.values():
+        if student_name in freeswitch_ids and freeswitch_ids[student_name] == id:
+            freeswitch_conference_cmd('undeaf', id)
+            freeswitch_conference_cmd('unmute', id)
+        else:
+            freeswitch_conference_cmd('deaf', id)
+            freeswitch_conference_cmd('mute', id)
+
+def unmute_student(student_name):
+    if student_name in freeswitch_ids:
+        freeswitch_conference_cmd('undeaf', freeswitch_ids[student_name])
+        freeswitch_conference_cmd('unmute', freeswitch_ids[student_name])
+
+def mute_student(student_name):
+    if student_name in freeswitch_ids:
+        freeswitch_conference_cmd('deaf', freeswitch_ids[student_name])
+        freeswitch_conference_cmd('mute', freeswitch_ids[student_name])
