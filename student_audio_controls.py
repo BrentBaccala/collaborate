752b9bd96c32fdb4466ec846f8eff8628e5b80bd
Brent Baccala <cosine@freesoft.org>
2020-05-01 11:51:27 -0400

    add first crack at student audio controls (does nothing, just displays icons)

---
+++ student_audio_controls.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import tkinter as tk
+
+window = tk.Tk()
+
+mute_logo = tk.PhotoImage(file="/home/baccala/src/osito/mute_filled_BigBlueButton.ppm")
+deaf_logo = tk.PhotoImage(file="/home/baccala/src/osito/listen_filled_BigBlueButton.ppm")
+
+mute_button = tk.Label(
+    text="MUTE",
+    image=mute_logo,
+    width=150,
+    height=150,
+    bg="white",
+    fg="black",
+)
+
+deaf_button = tk.Label(
+    text="DEAF",
+    image=deaf_logo,
+    width=150,
+    height=150,
+    bg="white",
+    fg="black",
+)
+
+mute_button.pack(side="left")
+deaf_button.pack(side="right")
+
+window.mainloop()
