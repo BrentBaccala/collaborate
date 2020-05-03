03de47a1a8a1531c838d1264dad09d6129191a27
Brent Baccala <cosine@freesoft.org>
2020-05-02 20:52:32 -0400

    add "raised hand" to student audio controls, and put them in upper right corner (now will replace the raise_hand.py script)

--- student_audio_controls.py
+++ student_audio_controls.py
@@ -22,6 +22,8 @@ mute_white_logo = tk.PhotoImage(file="/home/baccala/src/osito/mute_filled_BigBlu
 mute_blue_logo = tk.PhotoImage(file="/home/baccala/src/osito/mute_filled_BigBlueButton_blue.gif")
 deaf_white_logo = tk.PhotoImage(file="/home/baccala/src/osito/listen_filled_BigBlueButton.ppm")
 deaf_blue_logo = tk.PhotoImage(file="/home/baccala/src/osito/listen_filled_BigBlueButton_blue.gif")
+hand_white_logo = tk.PhotoImage(file="/home/baccala/src/osito/hand_BigBlueButton.png")
+hand_blue_logo = tk.PhotoImage(file="/home/baccala/src/osito/hand_BigBlueButton_blue.png")
 
 mute_button = tk.Label(
     text="MUTE",
@@ -35,6 +37,15 @@ mute_button = tk.Label(
 deaf_button = tk.Label(
     text="DEAF",
     image=deaf_white_logo,
+    width=150,
+    height=150,
+    bg="white",
+    fg="black",
+)
+
+hand_button = tk.Label(
+    text="HAND",
+    image=hand_white_logo,
     width=150,
     height=150,
     bg="white",
@@ -59,14 +70,32 @@ def set_correct_icon_status():
     except:
         pass
 
+hand_raised = False
+
+def handle_click(event):
+    global hand_raised
+    hand_raised = ~ hand_raised
+    if hand_raised:
+        hand_button.configure(bg='blue')
+        hand_button.configure(image=hand_blue_logo)
+    else:
+        hand_button.configure(bg='white')
+        hand_button.configure(image=hand_white_logo)
+
+hand_button.bind("<Button-1>", handle_click)
+
 mute_button.pack(side="left")
-deaf_button.pack(side="right")
+deaf_button.pack(side="left")
+hand_button.pack(side="left")
 
 # Center audio control widget at the bottom of the screen
 # window.geometry("+" + str(int(screen_width/2 - 150)) + "-0")
 
 # Put audio control widget at the bottom right of the screen
-window.geometry("-0-0")
+# window.geometry("-0-0")
+
+# Put audio control widget at the top right of the screen
+window.geometry("-0+0")
 
 set_correct_icon_status()
 
