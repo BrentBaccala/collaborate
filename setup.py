2d349968b8beda9aff76fee3dd115ef730c0f592
Brent Baccala <cosine@freesoft.org>
2020-09-06 13:33:09 -0400

    basic setup for a pip-installable Python module

---
+++ setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vnc-collaborate-tool",
+    version="0.0.1",
+    author="Brent Baccala",
+    author_email="cosine@freesoft.org",
+    description="Scripts to facilite VNC remote desktop collaboration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BrentBaccala/collaborate",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+    ],
+)
