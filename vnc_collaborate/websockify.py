41380050d847a1e5327f4f9959ac0d0eee24f47f
Brent Baccala <cosine@freesoft.org>
2020-10-30 11:09:21 -0400

    fall-though case in websockify now goes to a default desktop in /run/vnc, named with the BBB meetingID, and started on the 'default' user if not already running

--- vnc_collaborate/websockify.py
+++ vnc_collaborate/websockify.py
@@ -87,6 +87,13 @@ def new_websocket_client(self):
         print(repr(err))
         fullName = ''
 
+    try:
+        decoded = jwt.decode(JWT, bigbluebutton.securitySalt())
+        meetingID = decoded['bbb-meetingID']
+    except (jwt.PyJWTError, KeyError) as err:
+        print(repr(err))
+        meetingID = 'default'
+
     rfbport = bigbluebutton.fullName_to_rfbport(fullName)
     UNIXuser = bigbluebutton.fullName_to_UNIX_username(fullName)
 
@@ -194,6 +201,92 @@ def new_websocket_client(self):
                 self.server.unix_target = socket_fn
         else:
             self.server.unix_target = '/run/vnc/' + UNIXuser
+
+    else:
+
+        # If this user didn't exist in the database, use a default session,
+        # but a different one for each meeting, so that each meeting's screenshare
+        # is separate from any others.  I expect this to become unnecessary once we can
+        # do the screenshare using a VNC multiplexer instead of the current scheme.
+
+        UNIXuser = 'default'
+        rfbpath = '/run/vnc/' + meetingID
+
+        if not os.path.exists(rfbpath):
+
+            tigervnc_version = 10
+
+            if os.path.exists('/usr/bin/tigervncserver'):
+
+                # Prefer Tiger VNC if it's installed.  We don't need a custom script to disable authentication,
+                # it supports the RANDR extension, and the most recent versions (not the Ubuntu 18 version)
+                # support UNIX domain sockets, which will (eventually) eliminate the need for the socat below.
+
+                # We do need to set BlacklistThreshold high, since connections with AuthType None are treated
+                # as blacklistable connections, and after we've gotten five of them, the server starts rejecting
+                # connections.
+
+                if tigervnc_version < 10:
+                    subprocess.run(['sudo', '-u', UNIXuser, '-i', 'tigervncserver',
+                                    '-localhost', 'yes',
+                                    '-SecurityTypes', 'None',
+                                    '-BlacklistThreshold', '1000000'])
+                else:
+                    # use our own tigervncserver because of a bug in the system version
+                    # that waits for the server to be listening on a TCP port even
+                    # if you requested a UNIX domain socket via "-rfbunixpath"
+                    subprocess.run(['sudo', '-u', UNIXuser, '-i',
+                                    'python3', '-m', 'vnc_collaborate', 'tigervncserver',
+                                    '-localhost', 'yes',
+                                    '-rfbunixpath', rfbpath,
+                                    '-SecurityTypes', 'None',
+                                    '-AcceptPointerEvents=0',
+                                    '-AcceptKeyEvents=0',
+                                    '-BlacklistThreshold', '1000000'])
+
+            else:
+
+                # We use a modifed version of the tight VNC server script that starts a server with no authentication,
+                # since we're providing the authentication using the JSON Web Tokens.
+
+                tightvncserver = pkg_resources.open_binary(__package__, 'tightvncserver.pl')
+                subprocess.run(['sudo', '-u', UNIXuser, '-i', 'perl'], stdin=tightvncserver)
+
+            # Use our root sudo access to make the user's .Xauthority file readable by group 'bigbluebutton',
+            # which allows the teacher to project screen shares onto the student desktop.
+
+            subprocess.run(['sudo', 'chgrp', 'bigbluebutton', '/home/{}/.Xauthority'.format(UNIXuser)])
+            subprocess.run(['sudo', 'chmod', 'g+r', '/home/{}/.Xauthority'.format(UNIXuser)])
+
+            # I also want to allow local VNC connections, mainly for overlaying VNC viewers within
+            # the VNC desktops (this is how we do things like screen shares and letting the teacher
+            # observe all of the student desktops).  Since ssvncviewer will connect to a UNIX domain
+            # socket, a simple solution is to start a socat to relay UNIX domain connections from a
+            # socket in /run/vnc to the VNC server.  Set its group to allow teacher access.
+
+            # XXX there's a race condition here - the Perl script vncserver has started, but it
+            # might not have yet started Xvnc, which is what find_running_VNCserver looks for.
+            # The only time I've actually seen rfbport set to None is when the user didn't have
+            # a home directory and the vncserver failed completely for that reason.
+
+            if tigervnc_version < 10:
+                rfbport = find_running_VNCserver(UNIXuser)
+                if rfbport:
+                    subprocess.run(['sudo', 'mkdir', '-p', '/run/vnc'])
+                    subprocess.Popen(['sudo', '-b', 'socat',
+                                      'UNIX-LISTEN:{},fork,user={},group={},mode=775'.format(rfbpath, UNIXuser, 'bigbluebutton'),
+                                      'TCP4:localhost:'+str(rfbport)])
+                    while not os.path.exists(rfbpath):
+                        time.sleep(0.1)
+            else:
+                # Xvnc allows us to set the mode of its UNIX domain socket, but not its group,
+                # so we need to wait for it to appear and adjust things accordingly
+                while not os.path.exists(rfbpath):
+                    time.sleep(0.1)
+                subprocess.run(['sudo', 'chgrp', 'bigbluebutton', rfbpath])
+                subprocess.run(['sudo', 'chmod', 'g+rw', rfbpath])
+
+        self.server.unix_target = rfbpath
 
     # pass through to the "parent" class's version of this method
     old_new_websocket_client(self)
