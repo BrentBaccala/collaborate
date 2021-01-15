7369fa5344e2fff343af9248356ac9e11a399fe1
Brent Baccala <cosine@freesoft.org>
2021-01-15 16:11:35 -0500

    add scripts to manipulate BBB shared notes

---
+++ get_from_shared_notes_text.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+from urllib.parse import urlencode, quote_plus
+
+f = open("/usr/share/etherpad-lite/APIKEY.txt")
+key = f.read()
+f.close()
+
+payload = {'apikey' : key}
+result = urlencode(payload, quote_via=quote_plus)
+url = "http://localhost:9001/api/1.2.1/listAllPads?" + result
+response = requests.get(url)
+response.raise_for_status()
+#print(response.json())
+
+lastEdited = {}
+
+for padID in response.json()['data']['padIDs']:
+   payload = {'apikey' : key, 'padID' : padID}
+   result = urlencode(payload, quote_via=quote_plus)
+   url = "http://localhost:9001/api/1.2.1/getLastEdited?" + result
+   response2 = requests.get(url)
+   response2.raise_for_status()
+   #print(response2.json())
+   lastEdited[response2.json()['data']['lastEdited']] = padID
+
+times = sorted(lastEdited.keys())
+last_time = times[-1]
+#print(lastEdited[last_time])
+
+payload = {'apikey' : key, 'padID' : lastEdited[last_time]}
+result = urlencode(payload, quote_via=quote_plus)
+url = "http://localhost:9001/api/1/getText?" + result
+response = requests.get(url)
+response.raise_for_status()
+print(response.json()['data']['text'])
