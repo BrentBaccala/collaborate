303441fc9f172b31433c07c7e9cb0c21728667d0
Brent Baccala <cosine@freesoft.org>
2022-09-18 18:44:12 -0400

    mostly converted install-lambda.py to Python
    add a Makefile to do some of the stuff previously done by install-lambda

--- bbb-aws-hibernate/install-lambda.py
+++ bbb-aws-hibernate/install-lambda.py
@@ -1,140 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env python3
 #
-# Use AWS command line to install a lambda function to autostart an instance
+# Use AWS boto3 library to install a lambda function to autostart an instance
 
-if [[ -z "${AWS_PROFILE}" ]]; then
-    echo 'Specify an AWS profile name in the AWS_PROFILE environment variable'
-    exit 1
-fi
-
-if [[ $1 == 'delete' ]]; then
-    API_IDS=$(aws apigatewayv2 get-apis --output=json | jq -r '.Items[] | select(.Name=="login").ApiId')
-    for API_ID in $API_IDS; do
-       ROUTE_ID=$(aws apigatewayv2 get-routes --api-id $API_ID --output=json | jq -r '.Items[0].RouteId')
-       INTEGRATION_ID=$(aws apigatewayv2 get-integrations --api-id $API_ID --output=json | jq -r '.Items[0].IntegrationId')
-       aws apigatewayv2 delete-route --api-id $API_ID --route-id $ROUTE_ID
-       # delete-integration always gives an error that some route still exists
-       aws apigatewayv2 delete-integration --api-id $API_ID --integration-id $INTEGRATION_ID
-       aws apigatewayv2 delete-api --api-id $API_ID
-    done
-    aws lambda delete-function --function-name login
-    POLICY_ARN=$(aws iam list-policies --scope=Local --output=json | jq -r '.Policies[] | select (.PolicyName == "login").Arn')
-    aws iam detach-role-policy --role-name login --policy-arn $POLICY_ARN
-    aws iam delete-policy --policy-arn $POLICY_ARN
-    # Can't delete role because it's attached to an instance
-    # aws iam delete-role --role-name login
-    exit
-fi
-
-if [[ $1 == 'add-global' ]]; then
-
-    # The IAM policies and roles are global to all AWS regions
-
-    # THIS DOESN'T WORK:
-    #aws iam create-role --role-name login --assume-role-policy-document "$(cat lambda-role-policy)"
-
-    aws iam create-policy --policy-name login --policy-document "$(cat login-role-policy)"
-
-    # this appears to be global to all AWS regions
-    POLICY_ARN=$(aws iam list-policies --scope=Local --output=json | jq -r '.Policies[] | select (.PolicyName == "login").Arn')
-
-    aws iam attach-role-policy --role-name login --policy-arn $POLICY_ARN
-    aws iam attach-role-policy --role-name login --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
-fi
-
-UPDATE_FUNCTION=no
-
-if [[ ! -r aws-login/my-deployment-package.zip ]]; then
-
-    mkdir -p aws-login
-    cd aws-login
-
-    pip3 install --target package requests
-    # Need version 1.7.1 because newer ones require an 'algorithms' argument to jwt.decode() even if verify=False
-    pip3 install --target package pyjwt==1.7.1
-    pip3 install --target package serialization
-    # (last command will show "ERROR: launchpadlib 1.10.13 requires testresources, which is not installed.")
-    # cryptography uses a binary shared object, and can be quite tricky to get working on lambda
-    # See https://aws.amazon.com/premiumsupport/knowledge-center/lambda-python-package-compatible/
-    pip3 install --target package --platform manylinux2014_x86_64 --python-version=3.9 --implementation cp --only-binary=:all: --upgrade cryptography
-    pip3 install --target package dnspython
-    unzip -d package cffi-1.15.0-cp39-cp39-manylinux_2_12_x86_64.manylinux2010_x86_64.whl
-
-    cd package; zip -r ../my-deployment-package.zip .; cd ..
-    zip -g -j my-deployment-package.zip ../lambda_function.py
-
-    cd ..
-
-    UPDATE_FUNCTION=yes
-fi
-
-# If deployment package has already been built, but python code has been updated, update the deployment package
-
-if [[ lambda_function.py -nt aws-login/my-deployment-package.zip ]]; then
-    cd aws-login
-    echo zip -g -j my-deployment-package.zip ../lambda_function.py
-    zip -g -j my-deployment-package.zip ../lambda_function.py
-    cd ..
-    UPDATE_FUNCTION=yes
-fi
-
-URL=$(aws apigatewayv2 get-apis --output=json | jq -r '.Items[] | select(.Name == "login") | .ApiEndpoint')
-
-if [[ -z $URL ]]; then
-
-    # A lambda function is local to an AWS region
-
-    # I also increased the time limit on the lambda process to 60 sec.  This much time is needed because, when
-    # called from 'waitpage', we wait for the collaborate server to start and register its DNS, which can be slow.
-
-    aws lambda create-function --function-name login --role $(aws iam list-roles --output=json | jq -r '.Roles[] | select (.RoleName=="login").Arn') --runtime=python3.9 --zip-file=fileb://aws-login/my-deployment-package.zip --handler=lambda_function.lambda_handler --timeout=60
-
-    # local to an AWS region
-    FUNCTION_ARN=$(aws lambda list-functions --output=json | jq -r '.Functions[] | select(.FunctionName == "login").FunctionArn')
-
-    aws apigatewayv2 create-api --name login --protocol-type HTTP --target $FUNCTION_ARN --route-key 'ANY /login'
-
-    API_ID=$(aws apigatewayv2 get-apis --output=json | jq -r '.Items[] | select(.Name=="login").ApiId')
-
-    # aws apigatewayv2 create-integration --api-id=$API_ID --integration-type=AWS_PROXY --integration-method=POST --integration-uri=$FUNCTION_ARN --payload-format-version=2.0
-
-    # INTEGRATION_ID=$(aws apigatewayv2 get-integrations --api-id $API_ID --output=json | jq -r '.Items[0].IntegrationId')
-    INTEGRATION_URI=$(aws apigatewayv2 get-integrations --api-id $API_ID --output=json | jq -r '.Items[0].IntegrationUri')
-
-    # aws apigatewayv2 create-route --api-id=$API_ID --route-key='ANY /login' --target=integrations/$INTEGRATION_ID
-
-    ACCOUNT_ID=$(aws sts get-caller-identity --output=json | jq -r .Account)
-
-    aws lambda add-permission --function-name login --statement-id apigateway-get --action lambda:InvokeFunction \
-        --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:us-east-1:$ACCOUNT_ID:$API_ID/*/*/login"
-
-    # This adds another permission for us-west-1:
-
-    aws lambda add-permission --function-name login --statement-id apigateway-get-us-west-1 --action lambda:InvokeFunction \
-        --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:us-west-1:$ACCOUNT_ID:$API_ID/*/*/login"
-
-    #aws lambda add-permission --function-name login --statement-id apigateway-get --action lambda:InvokeFunction \
-    #    --principal apigateway.amazonaws.com --source-arn $INTEGRATION_URI
-
-    URL=$(aws apigatewayv2 get-apis --output=json | jq -r '.Items[] | select(.Name == "login") | .ApiEndpoint')
-fi
-
-echo $URL
-
-# Can't do both this and the setting the environment (below) back-to-back because "an update is in progress"
-
-if [[ $UPDATE_FUNCTION = yes ]]; then
-    aws lambda update-function-code --function-name login --zip-file fileb://aws-login/my-deployment-package.zip
-fi
-
-# Basically, this:  (but the quoting in bash is a bear)
-# aws lambda update-function-configuration --function-name login --environment "Variables={BUCKET=my-bucket,KEY=file.txt}"
-
-#python3 <<EOF
-echo <<EOF
+import os
+import sys
+import boto3
 import json
 
-environment = {
+if 'AWS_PROFILE' not in os.environ:
+    print('Specify an AWS profile name in the AWS_PROFILE environment variable')
+    exit()
+
+CONFIG = {
     'ts2l': {
         'fqdn': 'ts2lclassroom.org',
         'instances': ['i-037fed505747bcc6c'],
@@ -147,15 +24,84 @@ environment = {
     },
 }
 
-assert len(json.dumps(environment)) <= 4096
+assert len(json.dumps(CONFIG)) <= 4096
 
-import boto3
+environment = { "Variables" : {"CONFIG" : json.dumps(CONFIG)} }
+
+sts = boto3.client('sts')
+ACCOUNT = sts.get_caller_identity()['Account']
+print('Account', ACCOUNT)
+
+apigw = boto3.client('apigatewayv2')
+
+if len(sys.argv) > 1 and sys.argv[1] == 'delete-api':
+    try:
+        API_ID = next(item['ApiId'] for item in apigw.get_apis()['Items'] if item['Name'] == 'login')
+    except StopIteration:
+        print("API 'login' not found in this region")
+        sys.exit(1)
+
+    for ROUTE_ID in (item['RouteId'] for item in apigw.get_routes(ApiId=API_ID)['Items']):
+        print('Deleting route', ROUTE_ID)
+        apigw.delete_route(ApiId=API_ID, RouteId=ROUTE_ID)
+
+    # Doesn't seem to be needed; throws an exception claiming that the route hasn't been deleted
+    #for INTEGRATION_ID in (item['IntegrationId'] for item in apigw.get_integrations(ApiId=API_ID)['Items']):
+    #    print('Deleting integration', INTEGRATION_ID)
+    #    apigw.delete_integration(ApiId=API_ID, IntegrationId=INTEGRATION_ID)
+
+    #print(ROUTE_ID)
+    #apigw.delete_route(ApiId=API_ID, RouteId=ROUTE_ID)
+    #apigw.delete_integration(ApiId=API_ID, IntegrationId=INTEGRATION_ID)
+    print('Deleting API', API_ID)
+    apigw.delete_api(ApiId=API_ID)
+
+    #aws lambda delete-function --function-name login
+    #POLICY_ARN=$(aws iam list-policies --scope=Local --output=json | jq -r '.Policies[] | select (.PolicyName == "login").Arn')
+    #aws iam detach-role-policy --role-name login --policy-arn $POLICY_ARN
+    #aws iam delete-policy --policy-arn $POLICY_ARN
+
+if len(sys.argv) > 1 and sys.argv[1] == 'add-global':
+    pass
+    #aws iam create-policy --policy-name login --policy-document "$(cat login-role-policy)"
+
+    # this appears to be global to all AWS regions
+    #POLICY_ARN=$(aws iam list-policies --scope=Local --output=json | jq -r '.Policies[] | select (.PolicyName == "login").Arn')
+
+    #aws iam attach-role-policy --role-name login --policy-arn $POLICY_ARN
+    #aws iam attach-role-policy --role-name login --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+
+iam = boto3.client('iam')
+ROLE=next(item['Arn'] for item in iam.list_roles()['Roles'] if item['RoleName'] == 'login')
+#print('Role', ROLE)
+
 l = boto3.client('lambda')
-print(l.update_function_configuration(FunctionName = 'login',
-                                      Environment = { "Variables" : {"CONFIG" : json.dumps(environment)}}))
 
-EOF
+try:
+    URL = next(item['ApiEndpoint'] for item in apigw.get_apis()['Items'] if item['Name'] == 'login')
+    with open('aws-login/my-deployment-package.zip', 'rb') as f:
+        zipfile = f.read()
+    l.update_function_code(FunctionName = 'login', ZipFile=zipfile)
+    l.update_function_configuration(FunctionName = 'login', Environment = environment)
 
-# echo aws lambda update-function-configuration --function-name login --environment $QUOTED_ENVIRONMENT
+except StopIteration:
+    l.delete_function(FunctionName='login')
+    with open('aws-login/my-deployment-package.zip', 'rb') as f:
+        zipfile = f.read()
+    FUNCTION_ARN = l.create_function(FunctionName='login', Role=ROLE, Runtime='python3.9',
+                                     PackageType='Zip', Code={'ZipFile': zipfile},
+                                     Environment=environment,
+                                     Handler='lambda_function.lambda_handler', Timeout=60)['FunctionArn']
+    #print(FUNCTION_ARN)
 
-# aws lambda get-function-configuration --function-name login
+    API = apigw.create_api(Name='login', ProtocolType='HTTP', Target=FUNCTION_ARN, RouteKey='ANY /login')
+    API_ID = API['ApiId']
+    URL = API['ApiEndpoint']
+
+    REGION = boto3.DEFAULT_SESSION.region_name
+    STATEMENT_ID = 'apigateway-get-{}'.format(REGION)
+    SOURCE_ARN = "arn:aws:execute-api:{}:{}:{}/*/*/login".format(REGION, ACCOUNT, API_ID)
+    l.add_permission(FunctionName='login', StatementId=STATEMENT_ID, Action='lambda:InvokeFunction',
+        Principal='apigateway.amazonaws.com', SourceArn=SOURCE_ARN)
+
+print('URL', URL + '/login')
